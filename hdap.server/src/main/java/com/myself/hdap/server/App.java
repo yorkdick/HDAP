b4912e884738b002e4aa0275d81f0a12b887f6b0
ybo <ybo1@its.jnj.com>
2017-07-18 14:25:15 +0800

    init

---
+++ hdap.server/src/main/java/com/myself/hdap/server/App.java
@@ -0,0 +1,13 @@
+package com.myself.hdap.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
